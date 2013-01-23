026a2e8cf4d75958d193b6dff37d073ab02f8e8b
Douglas Gardner <douglas@chippy.ch>
2013-01-23 22:09:39 +0000

    added ~repo for a basic repository overview
    Config file doesn't work properly yet, though

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ Grabs interesting data from the GitHub API.
 ### Description
 
 This module takes some interesting information about Github and parses it in a pleasing manner.
+
+### Configuration
+#### defaultrepo
+When repository information is lacking from the command, this repository will be used.
 ### Commands
 #### ~repocount [user]
 Returns the number of public Github repositories for the specified user.
--- github.js
+++ github.js
@@ -16,6 +16,33 @@ var github = function(dbot) {
 			event.reply(event.params[1] + " has " + result.length + " public repositories.");
         //        event.reply(dbot.t('repocount',{'user': event.params[1], 'count' : result.length}));
             });
+        },
+        '~repo': function(event) {
+            var repo = "";
+            if (typeof event.params[1] == 'undefined') {
+            //    repo = dbot.config.github.defaultrepo;
+                repo = "reality/depressionbot";
+            } else {
+                repo = event.params[1];
+            }
+
+            var reqUrl = "https://api.github.com/";
+            reqUrl += "repos/" + repo;
+
+            request(reqUrl, function(error, response, body) {
+                var data = JSON.parse(body);
+                var str = "";
+                str += data["name"] + " is a ";
+                if (data["fork"] == true) {
+                    str += "forked ";
+                }
+                str += data["language"];
+                str += " repo with ";
+                str += data["open_issues"] + " unresolved issues. ";
+                str += "[" + data["forks"] + "F " + data["watchers"] + "W]";
+                
+                event.reply(str);
+            });
         }
     };
     this.commands = commands;
