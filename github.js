0c3dad484c268190694a2a30d7e4816cfa4446c2
Douglas Gardner <douglas@chippy.ch>
2013-01-25 12:23:29 +0000

    ALPHABETIC!

--- github.js
+++ github.js
@@ -7,40 +7,6 @@ var request = require('request'),
 
 var github = function(dbot) {
     var commands = {
-        '~repocount': function(event) {
-        // TODO: add handling for non existent user
-            var reqUrl = "https://api.github.com/users/" + event.params[1] + "/repos";
-            request(reqUrl, function(error, response, body) {
-            var result = JSON.parse(body);
-			event.reply(event.params[1] + " has " + result.length + " public repositories.");
-            });
-        },
-        '~repo': function(event) {
-            var repo = "";
-            if (typeof event.params[1] == 'undefined') {
-                repo = dbot.config.github.defaultrepo;
-            } else {
-                repo = event.params[1];
-            }
-
-            var reqUrl = "https://api.github.com/";
-            reqUrl += "repos/" + repo;
-
-            request(reqUrl, function(error, response, body) {
-                var data = JSON.parse(body);
-                var str = "";
-                str += data["name"] + " is a ";
-                if (data["fork"] == true) {
-                    str += "forked ";
-                }
-                str += data["language"];
-                str += " repo with ";
-                str += data["open_issues"] + " unresolved issues. ";
-                str += "[" + data["forks"] + "F " + data["watchers"] + "W]";
-                
-                event.reply(str);
-            });
-        },
         '~gstatus': function(event) {
             var reqUrl = "https://status.github.com/api/last-message.json";
             request(reqUrl, function(error,response,body){
@@ -82,7 +48,41 @@ var github = function(dbot) {
                     }
                 }
            });
-       }, 
+        }, 
+        '~repo': function(event) {
+            var repo = "";
+            if (typeof event.params[1] == 'undefined') {
+                repo = dbot.config.github.defaultrepo;
+            } else {
+                repo = event.params[1];
+            }
+
+            var reqUrl = "https://api.github.com/";
+            reqUrl += "repos/" + repo;
+
+            request(reqUrl, function(error, response, body) {
+                var data = JSON.parse(body);
+                var str = "";
+                str += data["name"] + " is a ";
+                if (data["fork"] == true) {
+                    str += "forked ";
+                }
+                str += data["language"];
+                str += " repo with ";
+                str += data["open_issues"] + " unresolved issues. ";
+                str += "[" + data["forks"] + "F " + data["watchers"] + "W]";
+                
+                event.reply(str);
+            });
+        },
+        '~repocount': function(event) {
+        // TODO: add handling for non existent user
+            var reqUrl = "https://api.github.com/users/" + event.params[1] + "/repos";
+            request(reqUrl, function(error, response, body) {
+            var result = JSON.parse(body);
+			event.reply(event.params[1] + " has " + result.length + " public repositories.");
+            });
+        }
     };
     this.commands = commands;
 
