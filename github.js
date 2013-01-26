641344374d68b0ee63fac26113c8ab70a47d9c43
Douglas Gardner <douglas@chippy.ch>
2013-01-26 12:00:06 +0000

    remove duped commented out code

--- github.js
+++ github.js
@@ -82,32 +82,6 @@ var github = function(dbot) {
                 }
            });
         }, 
-/*        '~repo': function(event) {
-            var repo = "";
-            if (typeof event.params[1] == 'undefined') {
-                repo = dbot.config.github.defaultrepo;
-            } else {
-                repo = event.params[1];
-            }
-
-            var reqUrl = "https://api.github.com/";
-            reqUrl += "repos/" + repo;
-
-            request(reqUrl, function(error, response, body) {
-                var data = JSON.parse(body);
-                var str = "";
-                str += data["name"] + " is a ";
-                if (data["fork"] == true) {
-                    str += "forked ";
-                }
-                str += data["language"];
-                str += " repo with ";
-                str += data["open_issues"] + " unresolved issues. ";
-                str += "[" + data["forks"] + "F " + data["watchers"] + "W]";
-                
-                event.reply(str);
-            });
-        },*/
         '~repocount': function(event) {
         // TODO: add handling for non existent user
             var reqUrl = "https://api.github.com/users/" + event.params[1] + "/repos";
